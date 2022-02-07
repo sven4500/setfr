00d5b4b6f7e388af7bc5c37ca3b0b4592d370fd6
sven4500 <sven4500@mail.ru>
2022-02-08 02:16:39 +0300

    Чтение данных из файла и отправка по сокету (уровень 3 модели OSI).

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,15 @@
 
 #if defined(_MSC_VER)
 
+#include <stdio.h> // fopen, fread
+#include <stdint.h> // uint16_t, uint32_t
 #include <Winsock2.h>
 #include <ws2ipdef.h> // IP_HDRINCL
 #include <Windows.h>
-#include "headers.h" // LPIPV4_HDR, LPUDP_HDR
+#include <errno.h> // EOK, ...
+#include <getopt.h> // getopt, optarg
+#include <headers.h> // LPIPV4_HDR, LPUDP_HDR
+
 #define EOK 0
 
 #elif defined(__linux__)
@@ -53,70 +58,96 @@ int main(int argc, char** argv)
 		printf("WSAStartup failed\n");
 		return EXIT_FAILURE;
 	}
-
-	/*DWORD dwBufferLen = 16384;
-	LPWSAPROTOCOL_INFO const lpProtocolInfo = (LPWSAPROTOCOL_INFO)malloc(dwBufferLen);
-	INT const iNumInfo = WSAEnumProtocols(NULL, lpProtocolInfo, &dwBufferLen);
-	free(lpProtocolInfo);*/
 #endif
 
-	int const BUF_MAX = 1200;
-	int8_t buf[BUF_MAX] = {};
-	int dwPayload = 32;
-	uint16_t wSrcePort = 8000, wDestPort = 8001;
+	char filename[MAX_PATH] = {};
+	int c = 0, osi_level = 0, port = 0;
+
+	while ((c = getopt(argc, argv, "hf:o:p:")) != -1)
+	{
+		switch (c)
+		{
+		case 'f':
+			strcpy(filename, optarg);
+			break;
+		case 'o':
+			osi_level = atoi(optarg);
+			break;
+		case 'p':
+			port = atoi(optarg);
+			break;
+		case 'h':
+			printf(
+				"setfr (send Ethernet frame) utility\n"
+				"-f [path] path to Ethernet frame containing file\n"
+				"-o [2..4] OSI level\n"
+				"-p [..65535] port number\n"
+				"-h - print this message\n"
+			);
+			break;
+		default:
+			break;
+		}
+	}
+
+	int const mtu = 1518;
+	uint8_t frame_buf[mtu] = {};
 	
-	LPIPV4_HDR const iphdr = (LPIPV4_HDR)&buf[0];
-	iphdr->ip_verlen = (4 << 4) | (sizeof(IPV4_HDR) / sizeof(uint32_t));
-	iphdr->ip_tos = 0;
-	iphdr->ip_totallength = htons(sizeof(IPV4_HDR) + sizeof(UDP_HDR) + dwPayload);
-	iphdr->ip_id = 0;
-	iphdr->ip_offset = 0;
-	iphdr->ip_ttl = 8; // Time-to-live is eight
-	iphdr->ip_protocol = IPPROTO_UDP;
-	iphdr->ip_checksum = 0;
-	iphdr->ip_srcaddr = inet_addr("127.0.0.1");
-	iphdr->ip_destaddr = inet_addr("127.0.0.1");
-	// Calculate checksum for IPv4 header
-	// The checksum() function computes the 16-bit one's
-	// complement on the specified buffer. See the IPHDRINC
-	// code sample on the companion CD for its implementation.
-	//iphdr->ip_checksum = checksum(v4hdr, sizeof(IPV4_HDR));
+	FILE* const fd = fopen(filename, "rb");
 
-	// Initialize the UDP header
-	LPUDP_HDR const udphdr = (LPUDP_HDR)&buf[sizeof(IPV4_HDR)];
-	udphdr->src_portno = htons(wSrcePort);
-	udphdr->dst_portno = htons(wDestPort);
-	udphdr->udp_length = htons(sizeof(UDP_HDR) + dwPayload);
-	//udphdr->udp_checksum = 0;
+	if (fd == NULL)
+	{
+		printf("failed to open file\n");
+		return EXIT_FAILURE;
+	}
 
-	char* data = (char*)&buf[sizeof(IPV4_HDR) + sizeof(UDP_HDR)];
-	strcpy(data, "Hello RAW sockets");
+	fseek(fd, 0, SEEK_END);
+	int const frame_size = ftell(fd);
+	fseek(fd, 0, SEEK_SET);
 
-	// Calculate the IPv4 and UDP pseudo-header checksum - this routine
-	// extracts all the necessary fields from the headers and calculates
-	// the checksum over it. See the iphdrinc sample for the implementation
-	// of Ipv4PseudoHeaderChecksum().
-	//udphdr->udp_checksum = Ipv4PseudoHeaderChecksum(v4hdr, udphdr, data, sizeof(IPV4_HDR) + sizeof(UDP_HDR) + dwPayload);
+	if (frame_size > mtu)
+	{
+		printf("Ethernet frame exceeds MTU\n");
+		return EXIT_FAILURE;
+	}
 
-	// Открываем "сырой" сокет для отправки данных.
-	int const sock = socket(AF_INET, SOCK_RAW, IPPROTO_RAW); // IPPROTO_UDP
+	fread(frame_buf, frame_size, 1, fd);
+	fclose(fd);
+
+	sockaddr_storage addr = {};
+	int sock = -1;
+	
+	// Открываем сокет для отправки данных в соответствие с указанным уровнем OSI.
+	if (osi_level == 2)
+	{
+
+	}
+	else if (osi_level == 3)
+	{
+		sock = socket(AF_INET, SOCK_RAW, IPPROTO_RAW); // IPPROTO_UDP
+		
+		// Сообщаем ОС что IP заголовок предоставим самостоятельно.
+		int optval = 1;
+		setsockopt(sock, IPPROTO_IP, IP_HDRINCL, (char*)&optval, sizeof(optval));
+
+		((sockaddr_in *)&addr)->sin_family = AF_INET;
+		//((sockaddr_in *)&addr)->sin_port = udphdr->dst_portno;
+		((sockaddr_in *)&addr)->sin_addr.s_addr = htons(port);
+	}
+	else
+	{
+		printf("bad OSI level specified\n");
+		return EXIT_FAILURE;
+	}
+
 	if (sock == -1)
 	{
 		printf("failed to create socket %d\n", sock);
 		return EXIT_FAILURE;
 	}
 
-	// Сообщаем ОС что IP заголовок предоставим самостоятельно.
-	int iOptVal = 1;
-	setsockopt(sock, IPPROTO_IP, IP_HDRINCL, (char*)&iOptVal, sizeof(iOptVal));
-
-	sockaddr_storage addr = {};
-	((sockaddr_in *)&addr)->sin_family = AF_INET;
-	((sockaddr_in *)&addr)->sin_port = udphdr->dst_portno;
-	((sockaddr_in *)&addr)->sin_addr.s_addr = iphdr->ip_destaddr;
-
-	int const iBytesSent = sendto(sock, buf, sizeof(IPV4_HDR) + sizeof(UDP_HDR) + dwPayload, 0, (sockaddr *)&addr, sizeof(addr));
-	printf("%d bytes sent\n", iBytesSent);
+	int const bytes_sent = sendto(sock, (char*)frame_buf, frame_size, 0, (sockaddr *)&addr, sizeof(addr));
+	printf("%d bytes sent\n", bytes_sent);
 
 #if defined(_MSC_VER)
 	closesocket(sock);
